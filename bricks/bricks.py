32eca990480f977cabb655604f36c03e1d5d6cc9
Campos Mateos, Pablo <pablocamposmateos@gmail.com>
2023-06-28 01:10:49 +0200

    [ADD] Use new font.

--- bricks/bricks.py
+++ bricks/bricks.py
@@ -1,25 +1,47 @@
 import pygame
 from pygame.locals import *
+
 from brick_libs.brick_lib import brick_library, get_brick_by_id
 from brick_libs.map_lib import map_library
-from utils import load_image, play_sound, calculate_score
+from brick_libs.ui_lib import (draw_buttons, draw_map_selection, draw_close_button, 
+                               handle_button_click, handle_map_selection_click, 
+                               buttons, map_selection_buttons, map_buttons)
+
 from constants import *
-from brick_libs.ui_lib import draw_buttons, draw_map_selection, draw_close_button, handle_button_click, handle_map_selection_click, buttons, map_selection_buttons, map_buttons
+from utils import load_image, play_sound, calculate_score
 
 pygame.init()
 
-win = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.NOFRAME)  # Modificado para desactivar el reescalado y maximizar
-pygame.display.set_caption('Bricks AI')  # Título de la ventana
+win = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.NOFRAME)
+pygame.display.set_caption('Bricks AI')
 
-font = pygame.font.Font(None, FONT_SIZE)
-button_font = pygame.font.Font(None, BUTTON_FONT_SIZE)
+arcade_font_path = 'media/fonts/source/arcade.ttf'
+font = pygame.font.Font(arcade_font_path, FONT_SIZE)
+button_font = pygame.font.Font(arcade_font_path, BUTTON_FONT_SIZE)
 
+current_map = map_library['basic']  
+selected_map = None  
+show_map_selection = False  
 
+def draw_window():
+    win.fill(DARK_GRAY)
 
+    pygame.draw.rect(win, LIGHT_GRAY, (50, 50, 200, 50))
+    pygame.draw.rect(win, LIGHT_GRAY, (300, 50, 200, 50))
+    pygame.draw.rect(win, LIGHT_GRAY, (550, 50, 200, 50))
 
-current_map = map_library['basic']  # El mapa que se está jugando
-selected_map = None  # El mapa que se ha seleccionado en la pantalla de selección de mapa
-show_map_selection = False  # Si se muestra o no la pantalla de selección de mapa
+    time_text = font.render('Tiempo: 00:00', True, LIGHT_BLUE)
+    lives_text = font.render('Vidas: 3', True, WHITE)
+    score_text = font.render('Score: 0', True, RED)
+    win.blit(time_text, (60, 60))
+    win.blit(lives_text, (310, 60))
+    win.blit(score_text, (560, 60))
+
+    pygame.draw.rect(win, current_map.map_color, (50, 150, 700, 900))
+
+    draw_buttons(win, buttons, button_font)
+    draw_map_selection(win, show_map_selection, map_selection_buttons, map_buttons, button_font)
+    draw_close_button(win)
 
 running = True
 while running:
@@ -28,7 +50,7 @@ while running:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             x, y = event.pos
-            if x > WINDOW_WIDTH - 50 and y < 50:  # Verificar si se hizo clic en el botón de cerrar
+            if x > WINDOW_WIDTH - 50 and y < 50:  
                 running = False
             elif show_map_selection:
                 for button, info in map_selection_buttons.items():
@@ -44,25 +66,7 @@ while running:
                     if info['rect'].collidepoint(x, y):
                         show_map_selection = handle_button_click(buttons, button)
 
-    win.fill(DARK_GRAY)
-
-    pygame.draw.rect(win, LIGHT_GRAY, (50, 50, 200, 50))
-    pygame.draw.rect(win, LIGHT_GRAY, (300, 50, 200, 50))
-    pygame.draw.rect(win, LIGHT_GRAY, (550, 50, 200, 50))
-
-    time_text = font.render('Tiempo: 00:00', True, LIGHT_BLUE)
-    lives_text = font.render('Vidas: 3', True, WHITE)
-    score_text = font.render('Puntuación: 0', True, RED)
-    win.blit(time_text, (60, 60))
-    win.blit(lives_text, (310, 60))
-    win.blit(score_text, (560, 60))
-
-    pygame.draw.rect(win, current_map.map_color, (50, 150, 700, 900))
-
-    draw_buttons(win, buttons, button_font)
-    draw_map_selection(win, show_map_selection, map_selection_buttons, map_buttons, button_font)
-    draw_close_button(win)  # Dibujar el botón de cerrar
-
+    draw_window()
     pygame.display.flip()
 
 pygame.quit()
