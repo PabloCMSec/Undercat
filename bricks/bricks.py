b0fc41b28e1e540c21810f3cdf695e6086cfc353
Campos Mateos, Pablo <pablocamposmateos@gmail.com>
2023-06-27 22:08:48 +0200

    [ADD] Map selector.

--- bricks/bricks.py
+++ bricks/bricks.py
@@ -20,8 +20,9 @@ light_blue = (173, 216, 230)
 orange = (255, 165, 0)
 green = (0, 255, 0)
 purple = (128, 0, 128)
+map_button_color = (50, 50, 50)
+selected_map_button_color = (80, 80, 80)
 
-# Ajustado para mantener la misma distancia entre los botones
 button_spacing = 30
 button_width = 150
 buttons = {
@@ -31,8 +32,18 @@ buttons = {
     'Log': {'color': white, 'rect': pygame.Rect(50 + 3*(button_width + button_spacing), 1100, button_width, 50)},
 }
 
-# Referencia al mapa básico
-current_map = map_library['basic']
+map_selection_buttons = {
+    'Aceptar': {'color': green, 'rect': pygame.Rect(350, 650, 100, 50)},
+    'Cancelar': {'color': red, 'rect': pygame.Rect(350, 700, 100, 50)},
+}
+
+map_buttons = {}
+for i, map_id in enumerate(map_library.keys()):
+    map_buttons[map_id] = {'color': map_button_color, 'rect': pygame.Rect(300, 250 + i * 60, 200, 50)}
+
+current_map = map_library['basic']  # El mapa que se está jugando
+selected_map = None  # El mapa que se ha seleccionado en la pantalla de selección de mapa
+show_map_selection = False  # Si se muestra o no la pantalla de selección de mapa
 
 running = True
 while running:
@@ -41,46 +52,66 @@ while running:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             x, y = event.pos
-            for button, info in buttons.items():
-                if info['rect'].collidepoint(x, y):
-                    if button == 'Mapa':
-                        print(map_library)  # Imprime la biblioteca de mapas cuando se presiona el botón de mapa
-                    else:
-                        print(f'{button} ha sido pulsado')
+            if show_map_selection:
+                for button, info in map_selection_buttons.items():
+                    if info['rect'].collidepoint(x, y):
+                        if button == 'Aceptar':
+                            if selected_map is not None:
+                                current_map = map_library[selected_map]
+                            show_map_selection = False
+                        elif button == 'Cancelar':
+                            show_map_selection = False
+                for map_id, info in map_buttons.items():
+                    if info['rect'].collidepoint(x, y):
+                        selected_map = map_id
+                        for id, button in map_buttons.items():
+                            button['color'] = map_button_color if id != selected_map else selected_map_button_color
+            else:
+                for button, info in buttons.items():
+                    if info['rect'].collidepoint(x, y):
+                        if button == 'Mapa':
+                            show_map_selection = True
+                        else:
+                            print(f'{button} ha sido pulsado')
 
-    # Llena el fondo con gris oscuro
     win.fill(dark_gray)
 
-    # Dibuja las cajas de tiempo, vidas y puntuación.
     pygame.draw.rect(win, light_gray, (50, 50, 200, 50))
-    pygame.draw.rect(win, light_gray, (300, 50, 200, 50))  # Nuevo cuadro de vidas
+    pygame.draw.rect(win, light_gray, (300, 50, 200, 50))
     pygame.draw.rect(win, light_gray, (550, 50, 200, 50))
 
-    # Dibuja el texto en las cajas.
     time_text = font.render('Tiempo: 00:00', True, light_blue)
-    lives_text = font.render('Vidas: 3', True, white)  # Nuevo texto de vidas
+    lives_text = font.render('Vidas: 3', True, white)
     score_text = font.render('Puntuación: 0', True, red)
     win.blit(time_text, (60, 60))
-    win.blit(lives_text, (310, 60))  # Dibuja el nuevo texto de vidas
+    win.blit(lives_text, (310, 60))
     win.blit(score_text, (560, 60))
 
-    # Dibuja el cuadro de juego con el color del mapa actual
     pygame.draw.rect(win, current_map.map_color, (50, 150, 700, 900))
 
-    # Dibuja los botones.
     mouse_pos = pygame.mouse.get_pos()
     for button, info in buttons.items():
         if info['rect'].collidepoint(mouse_pos):
-            # Invierte los colores si el ratón está encima
             pygame.draw.rect(win, info['color'], info['rect'])
             button_text = button_font.render(button, True, light_gray)
         else:
             pygame.draw.rect(win, light_gray, info['rect'])
             button_text = button_font.render(button, True, info['color'])
-
-        # Dibuja el texto del botón
         button_text_rect = button_text.get_rect(center=info['rect'].center)
         win.blit(button_text, button_text_rect)
+
+    if show_map_selection:
+        pygame.draw.rect(win, light_gray, (250, 200, 300, 600))
+        for button, info in map_selection_buttons.items():
+            pygame.draw.rect(win, info['color'], info['rect'])
+            button_text = button_font.render(button, True, dark_gray)
+            button_text_rect = button_text.get_rect(center=info['rect'].center)
+            win.blit(button_text, button_text_rect)
+        for map_id, info in map_buttons.items():
+            pygame.draw.rect(win, info['color'], info['rect'])
+            map_text = button_font.render(map_id, True, dark_gray)
+            map_text_rect = map_text.get_rect(center=info['rect'].center)
+            win.blit(map_text, map_text_rect)
 
     pygame.display.flip()
 
