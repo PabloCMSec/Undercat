fcac9d0409b5d2c9ae861c174dbdd2408c56846d
Campos Mateos, Pablo <pablocamposmateos@gmail.com>
2023-06-27 20:18:19 +0200

    [ADD] Bricks are loaded in map.

--- bricks/brick_libs/brick_lib.py
+++ bricks/brick_libs/brick_lib.py
@@ -4,6 +4,9 @@ class Brick:
         self.brick_type = brick_type
         self.brick_color = brick_color
         self.points_per_hit = points_per_hit  # Array con los puntos por cada toque
+    
+    def __repr__(self):
+        return f"Brick(id={self.brick_id}, type={self.brick_type}, color={self.brick_color}, points_per_hit={self.points_per_hit})"
 
 # Crear una biblioteca de bricks
 brick_red = Brick('red','normal', (255, 0, 0), [10])
@@ -13,5 +16,11 @@ brick_blue = Brick('blue','normal', (0, 0, 255), [10])
 brick_library = {
     brick_red.brick_id: brick_red,
     brick_green.brick_id: brick_green,
-    brick_blue.brick_id: brick_blue,
+    brick_blue.brick_id: brick_blue
 }
+
+def get_brick_by_id(brick_id:str) -> dict:
+    if brick_id in brick_library:
+        return brick_library[brick_id]
+    else:
+        return None
--- bricks/brick_libs/map_lib.py
+++ bricks/brick_libs/map_lib.py
@@ -1,17 +1,32 @@
 import json
-from brick_libs.brick_lib import brick_library
+from brick_libs.brick_lib import get_brick_by_id
+
+def _get_bricks_from_map(map):
+    final_map = []
+    for row in map:
+        final_col=[]
+        for i in row:
+            final_col.append(get_brick_by_id(i))
+        final_map.append(final_col)
+    return final_map  # Devuelve la matriz final de ladrillos
 
 def _get_map_from_path(path: str):
-    with open(path) as path:
-        return json.load(path)
+    with open(path) as file:
+        return json.load(file)
     
+def _fill_map_with_bricks(path:str):
+    loaded_map = _get_map_from_path(path)
+    loaded_bricks = _get_bricks_from_map(loaded_map['map'])  # Accede a la clave 'map'
+    loaded_map['map'] = loaded_bricks  # Asigna loaded_bricks a la clave 'map'
+    return loaded_map['map']  # Devuelve la matriz de ladrillos
+
 class Map:
     def __init__(self, map_id:str, map_type:str, map_color:str, map_path:str):
         self.map_id = map_id
         self.map_type = map_type
         self.map_color = map_color
         self.map_path = map_path
-        self.map = _get_map_from_path(map_path)
+        self.map = _fill_map_with_bricks(map_path)
 
     def __repr__(self):
         return f'Map(id={self.map_id}, map_type={self.map_type}, map_color={self.map_color}, map={self.map})'
